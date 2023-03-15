000f8275e8462efce067e49afba3e3bbfe586429
Igor-JCI <7.jciab.7@gmail.com>
2023-03-15 12:10:18 +0300

    01-Avatar+Message

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Message from "./Message";
+import s from "./Message.module.css"
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
@@ -11,16 +13,14 @@ function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
-
+            <div className={s.name}>homeworks 1</div>
             {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
